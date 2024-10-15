deda36dc66dd4bc7506a39204b16d628f481c6f0
ntacv <nta.ckn@gmail.com>
2024-10-15 19:10:29 +0700

    feat script intro and question

--- script.sh
+++ script.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
+# INTRODUCTION
+
 echo "Hello to the server generator"
-ls
+echo "You are in:"
+pwd
+echo "This script will run instances of "$1" with data in "$2" file, on "$3" VM servers"
+
+
+run_main(){
+    echo "main script"
+}
+
+
+while true; do
+    read -p "Do you want to continue? [Y/n]" yn
+    case $yn in
+        [Yy]* ) run_main; break;;
+        [Nn]* ) exit;;
+        * ) run_main; break;;
+    esac
+done
+
+
+
